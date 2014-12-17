366ea2eea4a014179e2223a90a1cf11942a87f7e
Harry Ganz <hganz@rsmas.miami.edu>
2014-12-17 14:27:24 -0500

    Set up model and migrations for year, including validations

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,2 +1,11 @@
 class Year < ActiveRecord::Base
+	# Relationships 
+	has_many :strata, :class_name => 'Strat'
+
+	# Validations
+	validates :year, :presence => true,
+	  :numericality => {
+	  	:only_integer => true,
+	  	:greater_than => 1986, 
+	  	:less_than => 9999}
 end
--- db/migrate/20141217191252_create_years.rb
+++ db/migrate/20141217191252_create_years.rb
@@ -1,7 +1,7 @@
 class CreateYears < ActiveRecord::Migration
   def change
     create_table :years do |t|
-
+      t.int :year
       t.timestamps
     end
   end
