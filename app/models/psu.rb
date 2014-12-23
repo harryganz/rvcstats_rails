08ad69262596e578b89f8947ba6b78878a3631d4
Harry Ganz <hganz@rsmas.miami.edu>
2014-12-23 13:34:07 -0500

    Modified uniqueness validator for psu_cd for region,year - Added belongs_to relationship with region, year - Sets region, year to stratum region, year before validation - Made year, region read-only - Added custom validation to make sure protected status of PSU matches stratum NOTE: User can set region, year before validation, but model will silently override user if it doesn't match stratum

--- app/models/psu.rb
+++ app/models/psu.rb
@@ -2,18 +2,25 @@ class Psu < ActiveRecord::Base
 	#Relationships
 	belongs_to :stratum, :class_name => 'Strat',
 	 :foreign_key => 'strat_id'
+	belongs_to :year
+	belongs_to :region
 	has_many :stations
+
+	#Callbacks
+	before_validation :match_year_and_region
+	 #TODO: Not the most elegant solution, but it works
+
+	#Read-only attributes
+	attr_readonly :year, :region
 
 	#Validations
 	validates :psu_cd, 
 	  :presence => true,
 	  :uniqueness => {
-	  	:scope => :stratum,
+	  	:scope => [:year, :region],
 	  	:message => 'must be 
-	  	unique within each stratum'
+	  	unique within each year/region'
 	  }
-	  #TODO: Make unique within year, region instead
-
 	validates :month,
 	  :presence => true,
 	  :numericality => {
@@ -36,6 +43,7 @@ class Psu < ActiveRecord::Base
 	  	:only_integer => true,
 	  	:greater_than_or_equal_to => 0
 	  }
+	  #TODO: Make sure zone matches stratum
 
 	validates :subregion_nr,
 	  :allow_blank => true,
@@ -43,12 +51,14 @@ class Psu < ActiveRecord::Base
 	  	:only_integer => true,
 	  	:greater_than_or_equal_to => 0
 	  }
+	  #TODO: Make sure subregion matches region
 
 	validates :mapgrid_nr,
 	  :allow_blank => true,
 	  :numericality => {
 	  	:only_integer => true
 	  }
+	  #TODO: Make sure mapgrid_nr is valid
 
 	validates :mpa_nr,
 	  :presence => true,
@@ -62,4 +72,26 @@ class Psu < ActiveRecord::Base
 	  :numericality => {
 	  	:only_integer => true
 	  }
+
+	# Custom Validation to check that protected status is the same as stratum
+	validate :valid_mpa_nr_for_stratum
+
+	private 
+		# Before validation set the year to the 
+		# stratum year and the region
+		# to the stratum region
+		def match_year_and_region
+			self.year = self.stratum.year
+			self.region = self.stratum.region
+		end
+
+		# On validation make sure that 
+		# the MPA_NR is valid for the stratum
+		def valid_mpa_nr_for_stratum
+			prot = self.mpa_nr > 0 ? 1:0
+			unless prot == self.stratum.protected
+				raise 'psu must have the same protected status as its 
+				stratum'
+			end
+		end
 end
