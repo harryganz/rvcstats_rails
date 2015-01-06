a0c167914ef6a0552c098b15a66824e355e5f11c
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-06 14:42:51 -0500

    Moved public functions out of private area in class

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -4,65 +4,6 @@ module Api
 		before_action :set_resource, :only => [:destroy, :show, :update]
 		respond_to :json
 
-		private
-
-		 # Returns the resource from the created instance variable
-      # @return [Object]
-      def get_resource
-        instance_variable_get("@#{resource_name}")
-      end
-
-      # Returns the allowed parameters for searching
-      # Override this method in each API controller
-      # to permit additional parameters to search on
-      # @return [Hash]
-      def query_params
-        {}
-      end
-
-      # Returns the allowed parameters for pagination
-      # @return [Hash]
-      def page_params
-        params.permit(:page, :page_size)
-      end
-
-      # The resource class based on the controller
-      # @return [Class]
-      def resource_class
-        @resource_class ||= resource_name.classify.constantize
-      end
-
-      # The singular name for the resource class based on the controller
-      # @return [String]
-      def resource_name
-        @resource_name ||= self.controller_name.singularize
-      end
-
-      # Only allow a trusted parameter "white list" through.
-      # If a single resource is loaded for #create or #update,
-      # then the controller for the resource must implement
-      # the method "#{resource_name}_params" to limit permitted
-      # parameters for the individual model.
-      def resource_params
-        @resource_params ||= self.send("#{resource_name}_params")
-      end
-
-      # Use callbacks to share common setup or constraints between actions.
-      def set_resource(resource = nil)
-        resource ||= resource_class.find(params[:id])
-        instance_variable_set("@#{resource_name}", resource)
-      end
-      # POST /api/{plural_resource_name}
-      def create
-        set_resource(resource_class.new(resource_params))
-
-        if get_resource.save
-          render :show, status: :created
-        else
-          render json: get_resource.errors, status: :unprocessable_entity
-        end
-      end
-
 # DELETE /api/{plural_resource_name}/1
 def destroy
   get_resource.destroy
@@ -89,6 +30,65 @@ end
 def update
   if get_resource.update(resource_params)
     render :show
+  else
+    render json: get_resource.errors, status: :unprocessable_entity
+  end
+end
+
+private 
+
+# Returns the resource from the created instance variable
+# @return [Object]
+def get_resource
+  instance_variable_get("@#{resource_name}")
+end
+
+# Returns the allowed parameters for searching
+# Override this method in each API controller
+# to permit additional parameters to search on
+# @return [Hash]
+def query_params
+  {}
+end
+
+# Returns the allowed parameters for pagination
+# @return [Hash]
+def page_params
+  params.permit(:page, :page_size)
+end
+
+# The resource class based on the controller
+# @return [Class]
+def resource_class
+  @resource_class ||= resource_name.classify.constantize
+end
+
+# The singular name for the resource class based on the controller
+# @return [String]
+def resource_name
+  @resource_name ||= self.controller_name.singularize
+end
+
+# Only allow a trusted parameter "white list" through.
+# If a single resource is loaded for #create or #update,
+# then the controller for the resource must implement
+# the method "#{resource_name}_params" to limit permitted
+# parameters for the individual model.
+def resource_params
+  @resource_params ||= self.send("#{resource_name}_params")
+end
+
+# Use callbacks to share common setup or constraints between actions.
+def set_resource(resource = nil)
+  resource ||= resource_class.find(params[:id])
+  instance_variable_set("@#{resource_name}", resource)
+end
+# POST /api/{plural_resource_name}
+def create
+  set_resource(resource_class.new(resource_params))
+
+  if get_resource.save
+    render :show, status: :created
   else
     render json: get_resource.errors, status: :unprocessable_entity
   end
