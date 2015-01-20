374953f283791059098c21b9804da0c01c34bd3a
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-20 11:02:57 -0500

    Renamed stratum param to strat

--- app/controllers/api/samples_controller.rb
+++ app/controllers/api/samples_controller.rb
@@ -1,6 +1,6 @@
 class Api::SamplesController < ApplicationController
   def index
-  	@samples = Sample.with_species(query_params[:species]).with_year(query_params[:year]).with_region(query_params[:region]).with_stratum(query_params[:stratum]).is_protected(query_params[:prot]).when_present(query_params[:present])
+  	@samples = Sample.with_species(query_params[:species]).with_year(query_params[:year]).with_region(query_params[:region]).with_stratum(query_params[:strat]).is_protected(query_params[:prot]).when_present(query_params[:present])
   end
 
   private
@@ -8,6 +8,6 @@ class Api::SamplesController < ApplicationController
 	def query_params
 		raise 'prot cannot have more than one argument' if params[:prot].present? && params[:prot].length > 1
 		raise 'present cannot have more than one argument' if params[:present].present? && params[:present].length > 1
-		params.permit(:species => [], :year => [], :region => [], :stratum => [], :prot => [], :present => [])
+		params.permit(:species => [], :year => [], :region => [], :strat => [], :prot => [], :present => [])
 	end
 end
