8e75e086059d3886a6f5ce38e216b7c1d59ceec1
Harry Ganz <hganz@rsmas.miami.edu>
2015-01-20 09:43:06 -0500

    Changed protected par to prot ...
    Parameter previously named protected renamed prot to be consistent with
    strats controller. Added errors if prot or present parameters have more
    than one argument

--- app/controllers/api/samples_controller.rb
+++ app/controllers/api/samples_controller.rb
@@ -1,11 +1,13 @@
 class Api::SamplesController < ApplicationController
   def index
-  	@samples = Sample.with_species(query_params[:species]).with_year(query_params[:year]).with_region(query_params[:region]).with_stratum(query_params[:stratum]).is_protected(query_params[:protected]).when_present(query_params[:present])
+  	@samples = Sample.with_species(query_params[:species]).with_year(query_params[:year]).with_region(query_params[:region]).with_stratum(query_params[:stratum]).is_protected(query_params[:prot]).when_present(query_params[:present])
   end
 
   private
 	# Whitelist queryable params
-	def query_params 
-		params.permit(:species => [], :year => [], :region => [], :stratum => [], :protected => [], :present => [])
+	def query_params
+		raise 'prot cannot have more than one argument' if params[:prot].present? && params[:prot].length > 1
+		raise 'present cannot have more than one argument' if params[:present].present? && params[:present].length > 1
+		params.permit(:species => [], :year => [], :region => [], :stratum => [], :prot => [], :present => [])
 	end
 end
