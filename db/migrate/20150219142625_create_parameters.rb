cfc1ca493e82353c3076ab38e0c1a437b78d2aba
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-19 09:54:40 -0500

    Added columns to parameters table

--- db/migrate/20150219142625_create_parameters.rb
+++ db/migrate/20150219142625_create_parameters.rb
@@ -1,8 +1,13 @@
 class CreateParameters < ActiveRecord::Migration
   def change
     create_table :parameters do |t|
-
+      t.float :length_at_capture
+      t.float :length_at_maturity
+      t.float :wlen_a
+      t.float :wlen_b
+      t.integer :animal_id
       t.timestamps
     end
+    add_index :parameters, :animal_id
   end
 end
