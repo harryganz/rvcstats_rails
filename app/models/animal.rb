03c137b87563f79f692fe33a834703fe3250439d
Harry Ganz <hganz@rsmas.miami.edu>
2015-02-19 09:57:10 -0500

    Added Parameter validations, relations, scopes

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,16 +1,17 @@
 class Animal < ActiveRecord::Base
 	#Relationships
 	has_many :samples
-	
+	has_one :parameter
+
 	#Validations
-	
+
 	CD_REGEX = /\A[A-Z]{3}\s{1}(?:[A-Z_]{4}|[A-Z]{3}[.]{1})\Z/
 	# 3 Caps + space + (4 Caps Or 3 Caps and a period)
 	SCI_REGEX = /\A[A-Z]{1}[a-z]+\s{1}(?:[a-z]+|sp\.)\Z/
 		#A capital followed by lowercase 1 space and the rest lowercase letters or sp.
 	COM_REGEX = /\A[A-Za-z\.\s]+\Z/
 
-	validates :comname, 
+	validates :comname,
 		:allow_blank => true,
 		:format => {
 			:with => COM_REGEX,
@@ -18,7 +19,7 @@ class Animal < ActiveRecord::Base
 		}
 
 	validates :species_cd, :presence => true,
-	  :format => {:with => CD_REGEX, 
+	  :format => {:with => CD_REGEX,
 	  :message => 'must be capitalized, with first 4 letters
 	  of the genus and first 3 of the species'},
 	  :uniqueness => true
--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -1,2 +1,26 @@
 class Parameter < ActiveRecord::Base
+  # Relationships
+  belongs_to :species, :class_name => 'Animal',
+   :foreign_key => :animal_id
+
+   # Validations
+   validates :length_at_capture, :allow_blank => true,
+    :numericality => {
+      :greater_than => 0
+    }
+
+    validates :length_at_maturity, :allow_blank => true,
+     :numericality => {
+       :greater_than => 0
+     }
+
+     validates :wlen_a, :allow_blank => true,
+      :numericality => true
+
+      validates :wlen_b, :allow_blank => true,
+       :numericality => true
+
+    # Scopes
+    scope :with_species, -> species {joins(:species).where(:animals =>
+      {species_cd: species}) if species.present?}
 end
