0fff244a3b19de7cb180a9258715af4ce4b1250b
Harry Ganz <hganz@rsmas.miami.edu>
2015-06-19 09:59:06 -0400

    Updated strats and sample migration
    Split samples table into psu, ssu, and samples tables
    
    Split strats table into strats and domains

--- db/migrate/20141217201900_create_strats.rb
+++ db/migrate/20141217201900_create_strats.rb
@@ -1,14 +1,20 @@
 class CreateStrats < ActiveRecord::Migration
   def change
-    create_table :strats do |t|
+    create_table :domains do |t|
       t.integer :year
       t.string :region
+      t.integer :richness
+      t.timestamps
+    end
+    create_table :strats do |t|
       t.string :strat
       t.integer :prot
       t.integer :ntot
       t.integer :grid_size
+      t.integer :richness
+      t.integer :domain_id
       t.timestamps
     end
-    add_index :strats, [:year, :region, :strat, :prot], :unique => true #composite index for rapid searhcing of unique strata
+    add_index :strats, :domain_id
   end
 end
--- db/migrate/20150109153008_create_samples.rb
+++ db/migrate/20150109153008_create_samples.rb
@@ -1,28 +1,40 @@
 class CreateSamples < ActiveRecord::Migration
-  def change 
+  def change
+   create_table :psus do |t|
+     t.integer :month
+     t.integer :day
+     t.string :primary_sample_unit
+     t.integer :zone_nr
+     t.integer :subregion_nr
+     t.integer :mapgrid_nr
+     t.integer :mpa_nr
+     t.integer :richness
+     t.integer :strat_id
+     t.timestamps
+   end
+   create_table :ssus do |t|
+     t.integer :station_nr
+     t.float :lat_degrees
+     t.float :lon_degrees
+     t.float :depth
+     t.float :underwater_visibility
+     t.string :habitat_cd
+     t.integer :richness
+     t.integer :psu_id
+     t.timestamps
+   end
    create_table :samples do |t|
-      t.integer :month
-      t.integer :day
-      t.string :primary_sample_unit
-      t.integer :zone_nr
-      t.integer :subregion_nr
-      t.integer :mapgrid_nr
-      t.integer :mpa_nr
       t.integer :time_seen
-      t.integer :station_nr
-      t.float :lat_degrees
-      t.float :lon_degrees
-      t.float :depth
-      t.float :underwater_visibility
-      t.string :habitat_cd
       t.float :num
       t.float :len
       t.integer :animal_id
-      t.integer :strat_id
+      t.integer :ssu_id
       t.timestamps
     end
     # Index foreign keys
     add_index :samples, :animal_id
-    add_index :samples, :strat_id
+    add_index :samples, :ssu_id
+    add_index :ssus, :psu_id
+    add_index :psus, :strat_id
   end
 end
