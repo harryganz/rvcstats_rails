1dc1e73cefcb094d249b6d56bc835906defee9ee
Harry Ganz <hganz@rsmas.miami.edu>
2015-07-02 08:50:20 -0400

    Updated controller to include new scopes
    Calls new scopes to select year and region from domain model

--- app/controllers/api/strats_controller.rb
+++ app/controllers/api/strats_controller.rb
@@ -1,13 +1,14 @@
 class Api::StratsController < ApplicationController
   def index
-  	@strata = Strat.where(query_params)
+  	@strata = Strat.where(query_params[:strat],
+      query_params[:prot]).with_year(
+      query_params[:year]).with_region(query_params[:region])
   end
 
   private
   	#Whitelist allowed parameters
   	def query_params
-  		raise 'prot cannot have more than one argument' if params[:prot].present?
-      && params[:prot].length > 1
+  		raise 'prot cannot have more than one argument' if params[:prot].present? && params[:prot].length > 1
   		params.permit(:strat => [], :region => [], :year => [], :prot => [])
   	end
 end
