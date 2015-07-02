d9b8a46c5e133b0e162607cbd52a6efbb61ff7fc
Harry Ganz <hganz@rsmas.miami.edu>
2015-07-02 08:45:53 -0400

    Added validations for benthic data
    Validations for benthic data added to ssu model

--- app/models/ssu.rb
+++ app/models/ssu.rb
@@ -53,4 +53,93 @@ class Ssu < ActiveRecord::Base
     :numericality => {
       :only_integer => true
     }
+
+  validates :max_hard_relief,
+    :allow_blank => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0
+    }
+
+  validates :max_soft_relief,
+    :allow_blank => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0
+    }
+
+  validates :avg_hard_relief,
+  :allow_blank => true,
+  :numericality => {
+    :greater_than_or_equal_to => 0
+  }
+
+  validates :hard_rel_pct_0,
+    :allow_blank => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0,
+      :less_than_or_equal_to => 100
+    }
+
+  validates :hard_rel_pct_1,
+    :allow_blank => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0,
+      :less_than_or_equal_to => 100
+    }
+
+  validates :hard_rel_pct_2,
+    :allow_blank => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0,
+      :less_than_or_equal_to => 100
+    }
+
+  validates :hard_rel_pct_3,
+    :allow_blank => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0,
+      :less_than_or_equal_to => 100
+    }
+
+  validates :hard_rel_pct_4,
+    :allow_blank => true,
+    :numericality => {
+      :greater_than_or_equal_to => 0,
+      :less_than_or_equal_to => 100
+    }
+
+  validates :pct_sand,
+  :allow_blank => true,
+  :numericality => {
+    :greater_than_or_equal_to => 0,
+    :less_than_or_equal_to => 100
+  }
+
+  validates :pct_hard_bottom,
+  :allow_blank => true,
+  :numericality => {
+    :greater_than_or_equal_to => 0,
+    :less_than_or_equal_to => 100
+  }
+
+  validates :pct_coral,
+  :allow_blank => true,
+  :numericality => {
+    :greater_than_or_equal_to => 0,
+    :less_than_or_equal_to => 100
+  }
+
+  validates :pct_octo,
+  :allow_blank => true,
+  :numericality => {
+    :greater_than_or_equal_to => 0,
+    :less_than_or_equal_to => 100
+  }
+
+  validates :pct_sponge,
+  :allow_blank => true,
+  :numericality => {
+    :greater_than_or_equal_to => 0,
+    :less_than_or_equal_to => 100
+  }
+
 end
